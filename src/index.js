ad151d9684114d1467fbc945644ebdce2c1e2fbb
walteronoh <walterkiprono81@gmail.com>
2022-01-03 19:17:10 +0300

    fix: remove unrequired options from loaders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.css";
 
 const Index = () => {
     return <>
-        <h1 className="header">Simple React app using webpack</h1>
+        <h1 className="header">Simple React App using webpack</h1>
     </>;
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,25 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const devMode = process.env.NODE_ENV == "development";
 
 module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                use: ["babel-loader"]
             },
             {
                 test: /\.css$/,
                 //NOTE: Webpack uses the loaders from the right.
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: "[name]_[local]_[hash:base64]",
-                            sourceMap: true,
-                            minimize: true
-                        }
-                    }
-                ]
+                use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader"],
             }
         ]
     },
@@ -35,6 +27,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./public/index.html",
             filename: "./index.html"
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    devtool: devMode ? "eval" : "source-map"
 };
