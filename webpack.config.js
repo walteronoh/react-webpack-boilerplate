3e505861141f655a221c9985919d740458837428
walteronoh <walterkiprono81@gmail.com>
2022-01-20 22:39:42 +0300

    chore: change package name

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ module.exports = {
         filename: "bundle.js"
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ["babel-loader"]
